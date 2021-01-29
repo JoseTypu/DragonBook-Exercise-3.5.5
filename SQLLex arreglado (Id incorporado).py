746a5b24424b0b844dd716261cac14e794fe7559
JoseTypu <78197437+JoseTypu@users.noreply.github.com>
2021-01-29 02:36:55 -0500

    SQLLex arreglado (Id incorporado)
    Se añadio el token ID

---
+++ SQLLex arreglado (Id incorporado).py
@@ -0,0 +1,45 @@
+from ply import lex
+
+tokens = (
+    'select',
+    'where',
+    'from',
+    'id',
+)
+
+tk = input ("Inserte: ")
+def t_select(t):
+    r"""(S|s)(E|e)(L|l)(E|e)(C|c)(T|t)"""
+    t.value = "SELECT"
+    return t
+
+def t_where(t):
+    r"""(W|w)(H|h)(E|e)(R|r)(E|e)"""
+    t.value = "WHERE"
+    return t
+
+def t_from(t):
+    r"""(F|f)(R|r)(O|o)(M|m)"""
+    t.value = "FROM"
+    return t
+
+def t_id(t):
+    r"""[A-Za-z][A-Za-z0-9_]"""
+    return t
+
+t_ignore = " \t\n"
+
+def t_newline(t):
+    r"""\n+"""
+    t.lexer.lineno += t.value.count('\n')
+
+def t_error(t):
+    print("Caracter no valido '%s' en linea %d" % (t.value[0], t.lexer.lineno))
+    t.lexer.skip(1)
+
+lexer = lex.lex()
+
+lexer.input(tk)
+
+for token in lexer:
+    print(token)
